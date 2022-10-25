5d33145d470ae3dd80d8f5f2f314593733141212
soyRicardoCastro <ricardocastro.dev@gmail.com>
2022-10-25 12:43:39 -0400

    feat(Root): initial page for see game stauts

---
+++ src/pages/Partidos.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout } from '../components'
+
+function Partidos() {
+  return (
+    <Layout>
+      <div>a</div>
+    </Layout>
+  )
+}
+
+export default Partidos
