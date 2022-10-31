d362aeb38c18e764602e410dcc0e42f799a4311f
soyRicardoCastro <ricardocastro.dev@gmail.com>
2022-10-31 13:29:23 -0400

    change api url

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export default axios.create(
   {
-    baseURL: 'http://192.168.100.70:4000'
+    baseURL: 'https://quiniela-api-production.up.railway.app'
   }
 )
 
-// https://quiniela-api-production.up.railway.app
+//  http://192.168.100.70:4000
